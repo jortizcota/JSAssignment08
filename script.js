57c1936dbf5200d37e914e0e3d16a88a7e3c0f26
Ortiz Cota <josephortizcota@my.whccd.edu>
2020-03-04 20:47:36 -0800

    Worte three funtions that change background color, text color, and greets the user.

--- script.js
+++ script.js
@@ -9,3 +9,42 @@
 // also want to call this function again when the user saves their preferences to
 // immediately apply them. Make sure you also notify the user somehow that the preferences
 // were saved.
+
+let nameField = document.getElementById("name");
+let greeting = document.getElementById("greeting");
+let backgroundColor = document.getElementById("background-color");
+let textColor = document.getElementById("foreground-color");
+
+const changeBackgroundColor = event => {
+  event.preventDefault();
+
+  let backgroundColorContents = backgroundColor.value;
+
+  let body = document.querySelector("body");
+
+  body.style.backgroundColor = backgroundColorContents;
+};
+
+const changeTextColor = event => {
+  event.preventDefault();
+
+  let textColorContents = textColor.value;
+
+  let body = document.querySelector("body");
+
+  body.style.color = textColorContents;
+};
+
+const myGreeting = event => {
+  event.preventDefault();
+
+  let nameFieldContents = nameField.value;
+
+  greeting.textContent = "Hello " + nameFieldContents;
+};
+
+let myForm = document.getElementById("preferences-form");
+
+myForm.addEventListener("submit", changeBackgroundColor);
+myForm.addEventListener("submit", changeTextColor);
+myForm.addEventListener("submit", myGreeting);
